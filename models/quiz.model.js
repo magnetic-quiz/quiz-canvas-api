ed89d9b8f81aaff69ac5337df344c3811eb4e2e6
magnetic-quiz <magneticquiz@gmail.com>
2024-09-30 11:56:39 +0530

    quiz model updated

--- models/quiz.model.js
+++ models/quiz.model.js
@@ -7,9 +7,35 @@ const QuizSchema = new Schema(
     questions: [
       {
         questionText: { type: String, required: true },
+        isMandatory: { type: Boolean },
         options: [
           {
             text: { type: String, required: true },
+          },
+        ],
+      },
+    ],
+    outcomes: [
+      {
+        title: { type: String, required: true },
+        description: { type: String },
+      },
+    ],
+    logic: [
+      {
+        questionID: {
+          type: Schema.Types.ObjectId,
+          ref: "Question",
+          required: true,
+        }, // Refers to a question in the quiz
+        optionOutcomes: [
+          {
+            optionID: { type: Schema.Types.ObjectId, required: true }, // Refers to the specific option for a question
+            outcomeID: {
+              type: Schema.Types.ObjectId,
+              ref: "Outcome",
+              required: true,
+            }, // Refers to the outcome mapped to this option
           },
         ],
       },
